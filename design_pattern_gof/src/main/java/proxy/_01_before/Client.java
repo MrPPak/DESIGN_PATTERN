c6473670a64347491d7289a252be6525e1507941
MrPPak <matht1115@naver.com>
2022-01-16 19:34:02 +0900

    feat: proxy before부분 구현

---
+++ design_pattern_gof/src/main/java/proxy/_01_before/Client.java
@@ -0,0 +1,9 @@
+package proxy._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        GameService gameService = new GameService();
+        gameService.startGame();
+    }
+}
---
+++ design_pattern_gof/src/main/java/proxy/_01_before/GameService.java
@@ -0,0 +1,13 @@
+package proxy._01_before;
+
+public class GameService {
+
+    public void startGame() {
+        System.out.println("이 자리에 오신 여러분을 진심으로 환영합니다.");
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
